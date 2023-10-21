7242c56ca2de85ce2bff499dbac86ea232e11e10
JackStras <jackstrasbaugh@yahoo.com>
2023-10-21 12:55:00 -0400

    Installed inquirer and added questions to array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require(inquirer)
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your application?", "What is the description of your application?", "What are the installation instructions?", "Describe any important usage information.", "Please list any collaberators.", "What are the instructions for testing your application?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
