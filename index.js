f8d21555e596e9a5c9c7875ecaf0ce0ec0bf8be7
JackStras <jackstrasbaugh@yahoo.com>
2023-10-25 14:48:29 -0400

    Added array of questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
-const inquirer = require(inquirer)
+const inquirer = require("inquirer")
+const fs = require("fs")
+const markdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your application?", "What is the description of your application?", "What are the installation instructions?", "Describe any important usage information.", "Please list any collaberators.", "What are the instructions for testing your application?"];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description for your project."
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Provide installation instructions."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide usage information"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Provide any contributors."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Provide instructions for testing your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license.",
+        choices: ["None,", "Apache License 2.0", "MIT License", "Mozilla Public License 2.0"]
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
